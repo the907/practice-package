a667b26413ee2cc74a990310eb1b716c4b8b40b4
the907 <the907@users.noreply.github.com>
2023-05-23 16:16:38 +0000

    Fix styling

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -2,11 +2,11 @@
 
 namespace AdCrafters\WeightConversions\Tests;
 
-use PHPUnit\Runner\Filter\Factory;
 class ExampleTest extends TestCase
 {
     /** @test */
-    public function it_can_test() {
+    public function it_can_test()
+    {
 
         $this->assertTrue(true);
     }
